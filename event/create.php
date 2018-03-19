619ad795812e3531dd59aa0d3db41fab2c41d7e3
charmming <charmming4u@gmail.com>
2018-03-19 11:19:52 +0100

    made changes to the event folder and adjusted the pages

--- event/create.php
+++ event/create.php
@@ -14,7 +14,7 @@
     <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-      <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+
 
       <script src="style.js"></script>
 <!-- scripts ends here -->

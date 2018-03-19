ba26cf0637a9a2cfd03592b1beac9c599683d040
Adetona <adetonaabiodun12@gmail.com>
2018-03-19 11:24:32 +0100

    created createclass.php

--- event/createclass.php
+++ event/createclass.php
@@ -3,15 +3,15 @@
 
  require_once('../Database/conn.php');
 
-if(isset($_POST['submit'])){
+if(isset($_POST['save'])){
 	
 $title = $_POST['title']; 
 $description = $_POST['description']; 
-$creator_id = $_SESSION['id']; 
+//$creator_id = $_SESSION['id']; 
 $created_at = date("Y-m-d"); //get the date of registration. 
 
 
-	$query = "INSERT into `event` (title, description, creator_id) VALUES ($title, $description, $creator_id, $created_at)";
+	$query = "INSERT into `event` (title, description) VALUES ($title, $description,  $created_at)";
 
         $result = mysqli_query($con,$query);//send the query
 

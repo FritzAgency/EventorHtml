93b74e98cfc2df64134b29f7cac79b28e07b59c6
charmming <charmming4u@gmail.com>
2018-03-25 07:04:41 +0100

    new generated page edited.

--- newGenerated.php
+++ newGenerated.php
@@ -7,8 +7,93 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  <link rel="stylesheet" href="bootstrap.min.css">
-  <link rel="stylesheet" href="style.css">
+  <script src="https://code.jquery.com/jquery-1.10.1.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script> 
+  <style>
+@import url('https://fonts.googleapis.com/css?family=Montserrat');
+
+body {
+    background-image: url("./img/eventor.jpg");
+    font-family: 'Montserrat', sans-serif;
+}
+
+ .nav-tabs > li.active > a, .nav-tabs > li.active > a:focus, .nav-tabs > li.active > a:hover {
+ border:0;
+ background-color:#fff;
+ color:#000000;
+ }
+
+ .nav-tabs > li > a{padding:10px 30px}
+  .nav-tabs > li.active {
+  border-bottom: 4px solid #4f2684;
+  color:#000000;
+  }
+
+  input::-moz-focus-inner { 
+  border: 0; 
+}
+/*
+.nav-pills > li.item1.active > a, .nav-pills > li.item1.active > a:focus, .nav-tabs > li.item1.active > a:hover {
+ border:0;
+ background-color:#fff;
+ color:#000000;
+ }
+
+ .nav-pills > li > a{padding:10px 30px}
+  .nav-pills > li.active {
+  color:#000000;
+  }
+
+.nav-pills > li.active > a, .nav-pills > li.active > a:focus, .nav-pills > li.active > a:hover {
+    color: #fff;
+    background-color: white;
+} */
+
+/* ul li a {
+ background-color: #00ff00;
+ color: green;
+}
+
+ul li a:hover {
+ background-color: #ff0000;
+ color: yellow;
+} */
+
+.sizing{
+  width: 285px;
+  font-size: 18px;
+    font-weight: bolder;
+    text-align: center;
+    color: #000000;
+    border-bottom-color: yellow;
+}
+
+a {
+  outline: 0;
+}
+
+.sizinga{
+  outline: 0;
+  color: #000000;
+  font-weight: bold;
+}
+
+.jumbotron .h1, .jumbotron h1 {
+    font-size: 73px;
+    background-color: white;
+}
+
+.container .jumbotron, .container-fluid .jumbotron {
+
+padding-right: 0px;
+padding-left: 0px;
+background-color: white;
+padding-top: 15px;
+padding-bottom: 10px;
+}
+</style>
+
 </head>
 <body>
 
@@ -49,3 +134,211 @@
   <!-- event title goes in here -->
 <div class="jumbotron"><h1 style="text-align: center;">A Trip To The Hills Of <br>Idanran</h1></div>
 <!-- event title ends here -->
+
+<!-- Nav tabs -->
+<ul class="nav nav-tabs" role="tablist">
+  <li class="active sizing"><a class="sizinga" href="#hometab" role="tab" data-toggle="tab">ABOUT</a></li>
+  <li class="sizing"><a class="sizinga" href="#javatab" role="tab" data-toggle="tab" >SPONSORS</a></li>
+  <li class="sizing"><a class="sizinga" href="#csharptab" role="tab" data-toggle="tab">LOCATION</a></li>
+  <li class="sizing"><a class="sizinga" href="#mysqltab" role="tab" data-toggle="tab">VENDORS</a></li>
+  
+</ul>
+</li>
+
+<!-- Tab panes -->
+<div class="tab-content">
+  <div class="tab-pane active" id="hometab" style="font-size: 17px; color: #626263;"><p style="text-align: justify;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, magni libero dicta corrupti mollitia odit temporibus! Doloribus hic rem explicabo illo quos alias minus molestiae. Atque modi ab sed illo quo repellat consequatur dicta, harum alias consequuntur ullam facilis officiis, tenetur officia dolorum cumque incidunt repellendus debitis accusamus molestiae cum adipisci explicabo neque pariatur. Culpa similique, voluptatum rerum a laborum soluta pariatur facilis, enim quo dicta, quisquam maxime ad. Adipisci officia maiores accusamus ea? Officiis sed rem accusantium, est, velit accusamus libero mollitia facere fuga aperiam nostrum earum? Vel asperiores quaerat necessitatibus iure, vitae repudiandae illo harum quisquam provident similique!</p></div>
+<p style="text-align: justify;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat quam reprehenderit aut nemo, sint quaerat illo non libero perferendis iusto mollitia? Temporibus facilis error optio! Optio cum esse facere vitae. Iusto incidunt enim neque repellendus, repudiandae doloremque excepturi alias est dolores, sed tempore, saepe odio. Quos reprehenderit obcaecati dicta numquam deserunt repellat, maxime facere officiis soluta eaque sed eligendi enim autem iusto asperiores voluptatem mollitia dignissimos porro pariatur! Odio voluptatum non eos itaque dolorem illum, perferendis ea exercitationem esse deserunt. Possimus nemo eligendi accusantium necessitatibus omnis, ipsa vel nostrum ab ipsam veniam voluptates officiis quia sint, laudantium officia impedit praesentium!</p>
+  
+  <div class="tab-pane" id="javatab">
+     <!-- sponsors logo will come here -->
+    <div style="margin-left: 74px; margin-right: auto; text-align: center; width: 100%; margin-top: 15px;">
+    <div class="row" style="margin-left: auto; margin-right:auto">
+      <div class="col-sm-2" style="height: 100px; background-color: grey; margin: 5px;">
+        sponsor 1
+      </div>
+      <div class="col-md-2" style="height: 100px; background-color: grey; margin: 5px;">
+        sponsor 2
+      </div>
+      <div class="col-md-2" style="height: 100px; background-color: grey; margin: 5px;">
+        sponsor 3
+      </div>
+      <div class="col-md-2" style="height: 100px; background-color: grey; margin: 5px;">
+        sponsor 4
+      </div>
+      <div class="col-md-2" style="height: 100px; background-color: grey; margin: 5px;">
+        sponsor 
+      </div>
+    </div>
+    <div class="row" style="margin-left: auto; margin-right:auto">
+      <div class="col-sm-2" style="height: 100px; background-color: grey; margin: 5px;">
+        sponsor 1
+      </div>
+      <div class="col-md-2" style="height: 100px; background-color: grey; margin: 5px;">
+        sponsor 2
+      </div>
+      <div class="col-md-2" style="height: 100px; background-color: grey; margin: 5px;">
+        sponsor 3
+      </div>
+      <div class="col-md-2" style="height: 100px; background-color: grey; margin: 5px;">
+        sponsor 4
+      </div>
+      <div class="col-md-2" style="height: 100px; background-color: grey; margin: 5px;">
+        sponsor 
+      </div>
+    </div>
+    </div>
+    <!-- sponsors contents ends here -->
+  </div>
+  <div class="tab-pane" id="csharptab">
+    <!-- google map goes in here -->
+    <h2>Address goes here and the map goes under</h2>
+
+    <img src="img/google_maps_hello_world.jpg" alt="" class="img-fluid" style="margin-left: auto; margin-right: auto; margin-top: 15px; width: 1139px;">
+    <!-- google map stops here -->
+  </div>
+  <div class="tab-pane" id="mysqltab">
+    <!-- sponsors logo will come here -->
+    <div style="margin-left: 74px; margin-right: auto; text-align: center; width: 100%; margin-top: 15px;">
+    <div class="row" style="margin-left: auto; margin-right:auto">
+      <div class="col-sm-2" style="height: 100px; background-color: grey; margin: 5px;">
+      vendor 1
+      </div>
+      <div class="col-md-2" style="height: 100px; background-color: grey; margin: 5px;">
+      vendor 2
+      </div>
+      <div class="col-md-2" style="height: 100px; background-color: grey; margin: 5px;">
+      vendor 3
+      </div>
+      <div class="col-md-2" style="height: 100px; background-color: grey; margin: 5px;">
+      vendor 4
+      </div>
+      <div class="col-md-2" style="height: 100px; background-color: grey; margin: 5px;">
+      vendor 
+      </div>
+    </div>
+    <div class="row" style="margin-left: auto; margin-right:auto">
+      <div class="col-sm-2" style="height: 100px; background-color: grey; margin: 5px;">
+      vendor 1
+      </div>
+      <div class="col-md-2" style="height: 100px; background-color: grey; margin: 5px;">
+      vendor 2
+      </div>
+      <div class="col-md-2" style="height: 100px; background-color: grey; margin: 5px;">
+      vendor 3
+      </div>
+      <div class="col-md-2" style="height: 100px; background-color: grey; margin: 5px;">
+      vendor 4
+      </div>
+      <div class="col-md-2" style="height: 100px; background-color: grey; margin: 5px;">
+        vendor 5
+      </div>
+    </div>
+    </div>
+    <!-- vendors contents stops in here -->
+  </div>
+
+<!-- menu tab for event information goes in here -->
+<!-- Nav tabs
+<div style="padding: 20px;"> -->
+<!-- <ul class="nav nav-tabs" role="tablist" style="border-bottom: 3px solid #d1d3d3; ">
+  <li class="active" style="font-size: 17px; width: 245px; text-align: center; margin-right: 20px; font-weight: bolder; color: black;"><a href="#hometab" role="tab" data-toggle="tab">ABOUT</a></li>
+  <li style="font-size: 15px; width: 245px; margin-right: 20px; text-align: center; font-weight: bolder; color: black;"><a href="#javatab" role="tab" data-toggle="tab">SPONSORS</a></li>
+  <li style="font-size: 15px; width: 245px; margin-right: 20px; font-weight: text-align: center; bolder; color: black;"><a href="#csharptab" role="tab" data-toggle="tab">LOCATION</a></li>
+  <li style="font-size: 15px; width: 245px; font-weight: bolder; margin-left: 20px; text-align: center; color: black;"><a href="#mysqltab" role="tab" data-toggle="tab">VENDORS</a></li>
+</ul>
+</li> -->
+
+<!-- Tab panes -->
+<!-- <div class="tab-content" style="margin-top: 15px; margin-left: auto; margin-right: auto;">
+  <div class="tab-pane active" id="hometab" style="f;ont-size: 17px; color: #626263">Write something for home tab</div>
+  <div class="tab-pane" id="javatab" style="font-size: 17px; color: #626263"> -->
+   
+    <!-- vensponsors logo will go here -->
+  <!-- </div> -->
+  <!-- <div class="tab-pane" id="csharptab" style="font-size: 17px; color: #626263">C# is also a programming language</div>
+  <div class="tab-pane" id="mysqltab" style="font-size: 17px; color: #626263"> -->
+    <!-- vendors logo will come here -->
+    <!-- <div style="margin: auto;">
+    <div class="row">
+      <div class="col-md-3" style="height: 100px; background-color: grey; margin: 5px;">
+        vendor 1
+      </div>
+      <div class="col-md-3" style="height: 100px; background-color: grey; margin: 5px;">
+        vendor 2
+      </div>
+      <div class="col-md-3" style="height: 100px; background-color: grey; margin: 5px;">
+        vendor 3
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-3" style="height: 100px; background-color: grey; margin: 5px;">
+        vendor 1
+      </div>
+      <div class="col-md-3" style="height: 100px; background-color: grey; margin: 5px;">
+        vendor 2
+      </div>
+      <div class="col-md-3" style="height: 100px; background-color: grey; margin: 5px;">
+        vendor 3
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-3" style="height: 100px; background-color: grey; margin: 5px;">
+        vendor 1
+      </div>
+      <div class="col-md-3" style="height: 100px; background-color: grey; margin: 5px;">
+        vendor 2
+      </div>
+      <div class="col-md-3" style="height: 100px; background-color: grey; margin: 5px;">
+        vendor 3
+      </div>
+    </div>
+    </div>
+    <!-- vendors logo will go here -->
+  <!-- </div>
+</div>
+
+</div> -->
+<!-- menu tab for event information ends here -->
+
+<!-- price tag goes in here -->
+<div class="row" style="width: 40%; margin: auto; margin-top: 20px;">
+  <div class="col-sm-3">
+    
+  </div>
+  <div class="col-sm-3" style="padding-top: 0px;">
+    <h3 style="color: #4f2684; font-weight: bold; font-size: 15px; text-align: center">PRICE</h3>
+  </div>
+  <div class="col-sm-4">
+    <div style="border: 3px solid lightgrey; text-align: center; border-radius: 17px;">
+      <h2 style="font-weight:bold; margin: 10px; font-size: 20px;">1500</h2>
+    </div>
+    <div class="col-sm-3">
+    <div style="border: 2px solid lightgrey; text-align: center; padding: 0px; margin: auto; border-radius: 43px;">
+
+    </div>
+  </div>
+</div>
+</div>
+<!-- price tag ends here -->
+
+<!-- menu tab for event information goes in here -->
+<!-- Nav tabs -->
+<div style="padding: 20px;">
+<ul class="nav nav-pills item1" role="tablist" style="border-bottom: 3px solid #d1d3d3;background-color: grey;">
+  <li class="active item1" style="font-size: 17px; width: 320px; margin-right: 20px; text-align: center; font-weight: bolder; margin-left: 10px; bottom-border-color-transparent;"><a href="#hometab1" role="tab" data-toggle="tab">Activities</a></li>
+  <li class="item1" style="font-size: 15px; width: 320px; margin-right: 20px; font-weight: text-align: center; bolder; margin-left: 10px; bottom-border-color-transparent;"><a href="#javatab1" role="tab" data-toggle="tab">Schedule</a></li>
+  <li class="item1" style="font-size: 15px; width: 320px; margin-right: 20px; font-weight: bolder; text-align: center; margin-left: 10px; bottom-border-color-transparent;"><a href="#csharptab1" role="tab" data-toggle="tab">Media</a></li>
+</ul>
+</li>
+
+<!-- Tab panes -->
+<div class="tab-content" style="margin-top: 15px;">
+  <div class="tab-pane active" id="hometab1" style="font-size: 17px; color: #626263">Write something for home tab</div>
+  <div class="tab-pane" id="javatab1" style="font-size: 17px; color: #626263">The Java is an object-oriented programming language <br /> that was developed by James Gosling from the Sun Microsystems in 1995.</div>
+  <div class="tab-pane" id="csharptab1" style="font-size: 17px; color: #626263">C# is also a programming language</div>
+</div>
+<!-- menu tab for event information ends here -->
+
+ </body>
+ </html>

5789ae6714d07fcbce8479f62e0dc01e1963c516
charmming <charmming4u@gmail.com>
2018-03-28 09:50:47 +0100

    updated version of form

--- newgenerated.php
+++ newgenerated.php
@@ -231,7 +231,21 @@ padding-bottom: 10px;
 
 </head>
 <body>
-
+<!-- nav bar starts here -->
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Creators Logo goes here</a>
+    </div>
+    <ul class="nav navbar-nav navbar-right">
+      <li class="active"><a href="#">Home</a></li>
+      <li><a href="#">Page 1</a></li>
+      <li><a href="#">Page 2</a></li>
+      <li><a href="#">Page 3</a></li>
+    </ul>
+  </div>
+</nav>
+<!-- nav bar ends here -->
 <div class="container" style="background-color: white; width: 60%;">
   <div id="myCarousel" class="carousel slide" data-ride="carousel" style=" border: 2px solid lightgrey;">
     <!-- Indicators -->

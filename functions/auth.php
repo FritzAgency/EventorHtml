f1e5f59047923fc8c343272cecd47cdfc7af28fa
Adetona <adetonaabiodun12@gmail.com>
2018-03-30 08:54:56 +0100

    error notice

--- functions/auth.php
+++ functions/auth.php
@@ -81,7 +81,7 @@ else{
    //throw error.
  //$_SESSION['message'] = 'something went wrong';   
 
-  echo "something went wrong" . mysqli_error();
+  echo "something went wrong" . mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 }
 
 }

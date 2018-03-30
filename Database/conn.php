2707ec13f32b7590121acc26553220a3bd43b945
Adetona <adetonaabiodun12@gmail.com>
2018-03-30 08:37:16 +0100

    Database path

--- Database/conn.php
+++ Database/conn.php
@@ -23,7 +23,7 @@ $url = parse_url(getenv("mysql://bacf3fbec39377:87d078f2@us-cdbr-iron-east-05.cl
 $server = $url["us-cdbr-iron-east-05.cleardb.net"];
 $username = $url["bacf3fbec39377"];
 $password = $url["87d078f2"];
-$db = substr($url["path"], 1);
+$db = substr($url["mysql://bacf3fbec39377:87d078f2@us-cdbr-iron-east-05.cleardb.net/heroku_adcba3479704be4?reconnect=true"], 1);
 
 $con = mysqli_connect($server, $username, $password, $db);
 

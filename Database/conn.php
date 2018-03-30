91824d4c1e7fdb4937a4c8c3de558d2c08807871
Adetona <adetonaabiodun12@gmail.com>
2018-03-30 08:40:30 +0100

    db conn

--- Database/conn.php
+++ Database/conn.php
@@ -23,9 +23,9 @@ $url = parse_url(getenv("mysql://bacf3fbec39377:87d078f2@us-cdbr-iron-east-05.cl
 $server = $url["us-cdbr-iron-east-05.cleardb.net"];
 $username = $url["bacf3fbec39377"];
 $password = $url["87d078f2"];
-$db = substr($url["mysql://bacf3fbec39377:87d078f2@us-cdbr-iron-east-05.cleardb.net/heroku_adcba3479704be4?reconnect=true"], 1);
+//$db = substr($url["mysql://bacf3fbec39377:87d078f2@us-cdbr-iron-east-05.cleardb.net/heroku_adcba3479704be4?reconnect=true"], 1);
 
-$con = mysqli_connect($server, $username, $password, $db);
+$con = mysqli_connect($server, $username, $password);
 
 if (mysqli_connect_errno())
   {

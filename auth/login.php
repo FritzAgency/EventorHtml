42d04e5012bc4c8ebc4a8b8379c08b964154a0d7
charmming <charmming4u@gmail.com>
2018-04-02 09:28:58 +0100

    search bar adjusted

--- auth/login.php
+++ auth/login.php
@@ -8,7 +8,7 @@
 	<link href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css" />
     <script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
     <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-    <link rel="stylesheet" href="app.css">
+    <!-- <link rel="stylesheet" href="app.css"> -->
     <link rel="stylesheet" href="signup.css">
 </head>
 <?php 

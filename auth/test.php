a998e30e3af353aea228c4504e806d42a5f82127
Adetona <adetonaabiodun12@gmail.com>
2018-04-02 12:04:53 +0100

    dropping database data

--- auth/test.php
+++ auth/test.php
@@ -2,36 +2,39 @@
     <head>
         ...
         <link rel="stylesheet" type="text/css" href="https://unpkg.com/file-upload-with-preview/dist/file-upload-with-preview.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.8.0/css/bootstrap-datepicker.css" />
         <!-- You'll want to make sure to at least set a width on the -->
         <!-- .custom-file-container class... -->
-        ...
+
+
+
+    
     </head>
     <body>
 
-        ...
+<form>
+    <div class="input-group date" data-provide="datepicker calendar">
+    <input type="text" class="form-control">
+    <div class="input-group-addon">
+        <span class="glyphicon glyphicon-th"></span>
+    </div>
+</div>
 
-        <div class="custom-file-container" data-upload-id="myUniqueUploadId">
-            <label>Upload File <a href="javascript:void(0)" class="custom-file-container__image-clear" title="Clear Image">Remove</a></label>
-
-            <label class="custom-file-container__custom-file" >
-                <input type="file" class="custom-file-container__custom-file__custom-file-input" accept="*" multiple>
-                <input type="hidden" name="MAX_FILE_SIZE" value="10485760" />
-                <span class="custom-file-container__custom-file__custom-file-control"></span>
-            </label>
-
-             <div class="custom-file-container__image-preview" style="height: 500px; width: 500px"> </div>
-
-
-
-        </div>
-
-        ...
-
-        <script src="https://unpkg.com/file-upload-with-preview"></script>
-        <script>
-            var upload = new FileUploadWithPreview('myUniqueUploadId')
-        </script>
+</form>
+        
 
     </body>
+<script type="text/javascript">
+    
+
+$('.datepicker').datepicker({
+    format: 'mm/dd/yyyy',
+    startDate: '-3d'
+});
+
+    </script>
+
 </html>

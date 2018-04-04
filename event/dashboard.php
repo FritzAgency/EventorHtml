a97a316193ddbe742ed0207671f3448f70f2a06f
charmming <charmming4u@gmail.com>
2018-04-04 10:43:12 +0100

    added vendor and create button to dashboard

--- event/dashboard.php
+++ event/dashboard.php
@@ -32,7 +32,9 @@
                 </button>
                 <a class="navbar-brand" href="../index.php" style="#a20eab"><img src="" alt="">user Logo</a> 
             </div>
-  <div style="color: white; padding: 15px 50px 5px 50px; float: right; font-size: 16px;"><a href="#" class="btn btn-danger square-btn-adjust" style="border: 2px solid gray; color: white; background-color: #4f2684; padding: 8px 20px; border-radius: 40px; font-size: 14px; font-weight: bold;">logout</a> </div>
+  <div style="color: white; padding: 15px 10px 5px 10px; float: right; font-size: 16px;"><a href="#" class="btn btn-danger square-btn-adjust" style="border: 2px solid gray; color: white; background-color: #4f2684; padding: 4px 20px; border-radius: 40px; font-size: 14px; font-weight: bold;">logout</a> </div>
+  <div style="color: white; padding: 15px 10px 5px 10px; float: right; font-size: 16px;"><a href="#" class="btn btn-danger square-btn-adjust" style="border: 2px solid gray; color: white; background-color: #4f2684; padding: 4px 20px; border-radius: 40px; font-size: 14px; font-weight: bold;">Become a Vendor</a> </div>
+  <div style="color: white; padding: 15px 10px 5px 10px; float: right; font-size: 16px;"><a href="page.php" class="btn btn-danger square-btn-adjust" style="border: 2px solid gray; color: white; background-color: #4f2684; padding: 4px 20px; border-radius: 40px; font-size: 14px; font-weight: bold;">Create Event</a> </div>
         </nav>   
            <!-- /. NAV TOP  -->
                 <nav class="navbar-default navbar-side" role="navigation" style="top: 60px;">

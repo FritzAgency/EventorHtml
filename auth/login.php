79b51854b54e7fb06478b8a2213e742186c8b616
charmming <charmming4u@gmail.com>
2018-04-04 10:13:44 +0100

    added login formating

--- auth/login.php
+++ auth/login.php
@@ -18,7 +18,49 @@
  ?>
 
 <body>
-<div class="container" style="margin-top: 100px;">
+
+<header>
+ <!-- header starts here -->
+ <nav class="navbar-sticky navbar navbar-default navbar-static-top" id="myTopnav" style="margin-bottom: 10px; background-color: white;">
+  <div class="container">
+    <div class="navbar-header" >
+      <div class="col-sm-2 col-md-3 col-xm-4" style="width: 90%;">
+      <a class="navbar-brand" href="../index.php"><img src="img/logo.png" alt="" srcset="" class="img-responsive" style="margin-top: -7px;"></a>
+      </div>
+    </div>
+    <ul class="nav navbar-nav navbar-right" class="topnav" id="myTopnav" style="margin-top: -41px;">
+    <!--li><a href="#" class="new">HOW IT WORKS </a></li>
+      <li><a href="#" class="new">EVENTS</a></li>
+      <li><a href="#" class="new">SPECIAL DEALS</a></li>
+      <li><a href="#" class="new">ABOUT US</a></li>
+      <li><a href="#" class="new">CONTACT</a></li-->
+      <!-- <li><a href="auth/signup.php" class="new">SIGNUP</a></li> -->
+      <!--li><a class="new"> | </a></li-->
+      <!-- <li><a href="auth/login.php" class="new">LOGIN</a></li> -->
+      <?php 
+
+      /*if ((!isset($_SESSION['email']))){
+
+
+  header("Location: ../auth/signup.php");
+
+} */ 
+
+ 
+?>
+
+
+
+
+<li style="    margin-top: 13px; margin-right: 17px; font-weight: bold; color: #4f2684; padding-top: 29px;"><a href="EventorHtml/index.php"><span class="glyphicon glyphicon-home" style="color: #4f2684; font-size: 21px;"></span></a></li> 
+
+    </ul>
+  </div>
+</nav>
+<!-- header stops here -->
+</header>
+
+<div class="container" style="margin-top: 87px; width: 29%; background-color: white;">
 
         <form class="form-horizontal" role="form" action="" method="POST">
 
@@ -65,5 +107,43 @@
                 </div>
 </form>
 </div>
+            </div>
+
+    <!-- footer goes here -->
+    <footer style="background-color: #4f2684; border: 3px solid #e7e7e7; padding: 10px; height: 50px; margin-top: 10%; margin-bottom: 10%">
+  <div class="row">
+    <div class="col-sm-2"  style="width: 12.666667%; margin-left: 36px">
+      <img src="img/footer.png" alt="" class="img-responsive" style="margin-top: 2px;">
+    </div>
+    <div class="col-sm-8" style="margin-top: -8px;">
+      <div class="row" style="width: 40%; margin-left: auto; margin-right: auto; margin-top: -4;">
+        <div class="col-sm-5">
+        <a href="#"><p style="text-align: center; font-weight: bold; padding-top: 15px; color: #ffffff; font-size: 10px;">How it works</p></a>
+        </div>
+        <div class="col-sm-4">
+        <a href="#"><p style="text-align: center; font-weight: bold; padding-top: 15px; color: #ffffff; font-size: 10px;">About us</p></a>
+        </div>
+        <div class="col-sm-3">
+        <a href="#"><p style="text-align: center; font-weight: bold; padding-top: 15px; color: #ffffff; font-size: 10px;">Contact</p></a>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-2"> 
+      <div class="row">
+        <div class="col-sm-4">
+          <a href=""><img src="img/index.png" alt="" style="width: 15px; height-max: 100%; margin-top: 10px;"></a>
+        </div>
+        <div class="col-sm-4">
+            <a href=""><img src="img/index1.png" alt="" style="width: 15px; height-max: 100%; margin-top: 10px;"></a>
+        </div>
+        <div class="col-sm-4">
+            <a href=""><img src="img/index11.png" alt="" style="width: 15px; height-max: 100%; margin-top: 10px;"></a>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
+<!-- footer ends here -->
+
 </body>
 </html>

d3c0ab092aadb76e77b7b1e964471a497a2a92f9
Adetona <adetonaabiodun12@gmail.com>
2018-04-16 10:13:52 +0100

    xxx

--- test1.php
+++ test1.php
@@ -18,8 +18,8 @@ $message = "<p>Hi,</p> <p>kindly help test the event rating system for fritz.</p
 
 $from = new SendGrid\Email('Abiodun Adetona', "abiodun@fritzng.com");
 $subject = "Fritz event review system.";
-$to = new SendGrid\Email(null, $email);
-$content = new SendGrid\Content("text/html", "abiodun@fritzng.com, adetonaabiodun12@gmail.com, adetona99@yahoo.com");
+$to = new SendGrid\Email(null, "abiodun@fritzng.com, adetonaabiodun12@gmail.com, adetona99@yahoo.com" );
+$content = new SendGrid\Content("text/html", $message);
 $mail = new SendGrid\Mail($from, $subject, $to, $content);
 
 //$apiKey = 'xxxx_api_key_xxxx';

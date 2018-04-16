019149ca09cfcfbf8976d84f4aeec8c837e0cd07
Adetona <adetonaabiodun12@gmail.com>
2018-04-16 13:33:37 +0100

    xxx

--- test1.php
+++ test1.php
@@ -16,9 +16,9 @@ $message = "<p>Hi,</p> <p>kindly help test the event rating system for fritz.</p
 //$email =  array("abiodun@fritzng.com, adetonaabiodun12@gmail.com, adetona99@yahoo.com");
 
 
-$from = new SendGrid\Email('Abiodun Adetona', "adetonaabiodun12@gmail.com");
+$from = new SendGrid\Email('Abiodun Adetona', "abiodun@fritzng.com");
 $subject = "Fritz event review system.";
-$to = new SendGrid\Email(null, "adetonaabiodun12@gmail.com");
+$to = new SendGrid\Email(null, "lawaltolu22@gmail.com");
 $content = new SendGrid\Content("text/html", $message);
 $mail = new SendGrid\Mail($from, $subject, $to, $content);
 

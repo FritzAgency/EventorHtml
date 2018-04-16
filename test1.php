28b838080e83b35abafebfa2b6e3c0e056989020
Adetona <adetonaabiodun12@gmail.com>
2018-04-16 11:44:04 +0100

    Finish up

--- test1.php
+++ test1.php
@@ -8,7 +8,7 @@ require 'vendor/autoload.php';
 // require("path/to/sendgrid-php/sendgrid-php.php");
 
 
-$message = "<p>Hi,</p> <p>kindly help test the event rating system for fritz.</p> <p> Click on this <a href='dev-eventor.herokuapp.com/rate.php'> link <a> to test the rating system </p> <p> Thank you! </p> 
+$message = "<p>Hi,</p> <p>kindly help test the event rating system for fritz.</p> <p> Click on this <a href='dev-eventor.herokuapp.com/rate.php'> link </a> to test the rating system </p> <p> Thank you! </p> 
 
 <p>Abiodun Adetona, Web developer @fritz</p> 
 "; 
@@ -16,9 +16,9 @@ $message = "<p>Hi,</p> <p>kindly help test the event rating system for fritz.</p
 //$email =  array("abiodun@fritzng.com, adetonaabiodun12@gmail.com, adetona99@yahoo.com");
 
 
-$from = new SendGrid\Email('Abiodun Adetona', "abiodun@fritzng.com");
+$from = new SendGrid\Email('Abiodun Adetona', "eniola.lion@gmail.com");
 $subject = "Fritz event review system.";
-$to = new SendGrid\Email(null, "adetonaabiodun12@gmail.com" );
+$to = new SendGrid\Email(null, "abiodun@fritzng.com" );
 $content = new SendGrid\Content("text/html", $message);
 $mail = new SendGrid\Mail($from, $subject, $to, $content);
 
